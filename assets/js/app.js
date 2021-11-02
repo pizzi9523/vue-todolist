e79a9a37307fc012487a894a5269ab8d4277a7a7
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-02 12:23:49 +0100

    add input and button to add task with min 5 char

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const app = new Vue({
     el: "#root",
     data: {
+        newTask: "",
+        error: false,
         tasks: [
             "Fare i compiti",
             "Fare la spesa",
@@ -10,7 +12,21 @@ const app = new Vue({
     },
     methods: {
         deleteTask(index) {
-            this.tasks.splice(index, 1)
+            this.tasks.splice(index, 1);
+        },
+
+        addNewTask() {
+            //console.log("add task");
+            //console.log(this.newTask);
+
+            if (this.newTask.length > 5) {
+                this.tasks.push(this.newTask);
+                this.error = false;
+            } else {
+                console.log("error");
+                this.error = true;
+            }
+            this.newTask = "";
         }
     }
 })
