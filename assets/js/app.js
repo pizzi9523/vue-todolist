be2cd830e29327b20bcec37e002647677cca1c41
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-02 11:54:34 +0100

    stampo un item per ogni elemento dell'array tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 const app = new Vue({
     el: "#root",
     data: {
+        tasks: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato"
+        ]
 
     }
 })
