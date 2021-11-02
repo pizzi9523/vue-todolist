17ef608c85f520d28445412dce29616a738958c4
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-02 15:25:05 +0100

    aggiungo lista completed al click dell'icona

---
+++ Bonus/assets/js/app.js
@@ -0,0 +1,47 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+        logoImg: "https://www.boolean.careers/images/misc/logo.png",
+        newTask: "",
+        error: false,
+        tasks: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato"
+        ],
+        tasksComplete: [
+
+        ]
+
+    },
+    methods: {
+        deleteTask(index) {
+            this.tasks.splice(index, 1);
+        },
+
+        addNewTask() {
+            //console.log("add task");
+            //console.log(this.newTask);
+
+            if (this.newTask.length > 5) {
+                this.tasks.push(this.newTask);
+                this.error = false;
+            } else {
+                // console.log("error");
+                this.error = true;
+            }
+            this.newTask = "";
+        },
+
+        modifyTask(index) {
+            console.log(this.tasks[index]);
+            let modifica = prompt("Modifica la tua task")
+            this.tasks.splice(index, 1, modifica)
+        },
+
+        addTaskCompleted(task) {
+            this.tasksComplete.push(task)
+            this.deleteTask()
+        }
+    }
+})
